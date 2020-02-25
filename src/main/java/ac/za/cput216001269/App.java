031860ab919b3c7066d00759ab9d8b5f8ed38be2
JMichaelson <aelmick92@gmail.com>
2020-02-25 23:15:38 +0200

    Added 5 methods tested
    #1 Object Equality
    
    #2 Object Identity
    
    #3 Failing Test
    
    #4 timeout test
    
    #5 Disability test

--- src/main/java/ac/za/cput216001269/App.java
+++ src/main/java/ac/za/cput216001269/App.java
@@ -6,8 +6,25 @@ package ac.za.cput216001269;
  */
 public class App 
 {
-    public static void main( String[] args )
+    private String input;
+    public App()
     {
-        System.out.println( "Hello World!" );
+
     }
+    public App(String n)
+    {
+        this.input = n;
+    }
+
+    public boolean comparedEqualityOfTwoObjects(Object object1, Object object2)
+    {
+        return object1.equals(object2);
+    }
+
+    public boolean comparedIdentityOfTwoObjects(Object o1, Object o2)
+    {
+        return o1 == o2;
+    }
+
+
 }
--- src/test/java/ac/za/cput216001269/AppTest.java
+++ src/test/java/ac/za/cput216001269/AppTest.java
@@ -1,20 +1,59 @@
 package ac.za.cput216001269;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.*;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
+    private App obj1, obj2, obj3;
+
+    @Before
+    public void setUp() throws Exception
     {
-        assertTrue( true );
+        obj1 = new App("Iphone6");
+        obj2 = new App("Iphone7");
+        obj3 = obj2;
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+    @Test
+    public void comparedObjectEquality()
+    {
+        boolean answer = new App().comparedEqualityOfTwoObjects(obj1, obj2);
+        Assert.assertSame(false, answer);
+    }
+
+    @Test
+    public void comparedTwoObjectsIdentity()
+    {
+        boolean compare = new App().comparedIdentityOfTwoObjects(obj2, obj3);
+        Assert.assertSame(true, compare);
+    }
+
+    @Test
+    public void failingTest()
+    {
+        Assert.fail("Test Failed!!!");
+    }
+
+    @Test(timeout = 20)
+    public void timeoutTest()
+    {
+        for (int i = 0; i < 5000; i++)
+        {
+            String name;
+        }
+    }
+
+    @Test
+    @Ignore
+    public void disablingTest()
+    {
+        boolean answer = new App().comparedEqualityOfTwoObjects(obj1, obj3);
+        Assert.assertSame(true, answer);
     }
 }
